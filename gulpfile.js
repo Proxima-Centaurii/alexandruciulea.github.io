57355baf3fdb651933e9d6066ebbd352aaa0b467
proxima-centaurii <alexandru.ciulea97@gmail.com>
2023-06-24 10:19:04 +0100

    Changed project configuration
    Initialised project with NPM, added gulp, sass, and gitignore file.

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+const { src, dest, watch, series} = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+
+function buildStyles(){
+    return src('assets/sass/**/*.scss')
+        .pipe(sass())
+        .pipe(dest('assets/css'))
+}
+
+function watchTasks(){
+    watch(['assets/sass/**/*.scss'], buildStyles)
+}
+
+exports.default = series(buildStyles, watchTasks)
